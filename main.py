087e7bf55281ab9d21d0c59e9e17e66b775fbdc4
Dag Hjelm <dag.hjelm@gmail.com>
2024-04-15 11:48:54 +0200

    add pseudo code comments

--- main.py
+++ main.py
@@ -33,4 +33,62 @@ if __name__ == "__main__":
     # index_doc(podcast_path)
 
     # client.bulk(operations=clean_sections, index="podcasts")
-    print(client.search(index="podcasts", q="cried on spot")['hits']['hits'][0]['_source']['transcript'])
+    # print(client.search(index="podcasts", q="cried on spot")['hits']['hits'][0]['_source']['transcript'])
+    # print(client.search(index="podcasts", q="cried on spot")['hits']['hits'][0]['_source'])
+    # query = {
+    #     "query": {
+    #         "match_phrase": {
+    #             "transcript": "your exact phrase"
+    #         }
+    #     }
+    # }
+    phrase_query = {
+        "query": {
+            "match_phrase" : {
+                "field": "SEarch"
+            }
+        }
+    }
+    intersection_query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"transcript": "Alice"}},
+                    {"match": {"time_stamp": "13:37"}}
+                ]
+            }
+        }
+    }
+    #hits = client.search(index="podcasts", q="starttime")['hits']['hits']
+    # hits = client.search(index="podcasts", q=query)['hits']['hits']
+    resp = client.search(
+        index="podcasts",
+        pretty=True,
+        body={"query": {"match_phrase": {"transcript": "I almost on the spot"}}},
+        # body={"query": {"match_phrase": "I almost cried on the spot"}},
+    )
+
+    # print(client.search(index="podcasts", q="I almost cried on spot"))
+
+    print(resp)
+
+    # ------------------------------------
+    # pseudo code for getting final results
+    # ------------------------------------
+
+    # def get_time_of_word(words, match):
+    #     for word in words:
+    #         if word == match:
+    #             return word.start_time
+    #     return None
+
+    # search_term = "I almost cried on the spot"
+    # id = resp.id 
+    # podcast_name = resp.podcast_name
+    # start_time = get_time_of_word(resp.words, search_term[0])
+    # returned_transcript = client.get(id - 1).transcript + resp.transcript + client.get(id + 1).transcript
+    # returned_start_time = start_time
+
+    # --------------------------------
+    # pseudo code ends
+    # ------------------------------
