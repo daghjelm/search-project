a2a9ce2473ce97ef11bd19151802591fa6d2cee4
Dag Hjelm <dag.hjelm@gmail.com>
2024-04-29 15:30:43 +0200

    concatenation of transcripts

--- main.py
+++ main.py
@@ -41,14 +41,42 @@ def get_words_from_episode(es, episode_id, query):
 
     return resp["hits"]["hits"]
 
+def get_section_by_id(es, section_id):
+    resp = es.search(
+        index="words",
+        pretty=True,
+        query={"term": {"_id": section_id}},
+    )
+
+    return resp["hits"]["hits"][0]['_source']
+
+def concatenate_until_time(es, section_id, start_time, snipped_length):
+    pointer = 1
+    total_time = 0
+    transcript = ""
+    direction = 1
+    while (total_time < snipped_length):
+        section = get_section_by_id(es, section_id + pointer * direction)
+        total_time = section['words'][-1]['endTime'] - start_time
+        transcript += section['transcript']
+        direction *= -1
+        if direction == 1:
+            pointer += 1
+    
+    return transcript
+
+
 if __name__ == "__main__":
-    s = "We don't know what it is or where it's going yet"
+    s = " backflip. We rock paper scissors"
 
     episode_hits = episode_from_query(es, s)
-    print('first episode hit:', episode_hits[0]['_source']['episode'])
-    print('---------')
 
-    section_hits = get_words_from_episode(es, episode_hits['episode'], s)
+    episode_id = episode_hits[0]['_source']['episode']
+
+    section_hits = get_words_from_episode(es, episode_id, s)
+    first_hit = section_hits[0]
+    first_hit_id = first_hit['_id']
+
     for hit in section_hits:
         print('hit:')
         print(hit["_source"]["transcript"])
