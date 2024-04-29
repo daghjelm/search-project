52565d279aac8b5c989da4fb0d9fb8c6a44779fb
Dag Hjelm <dag.hjelm@gmail.com>
2024-04-29 09:25:32 +0200

    and queries

--- main.py
+++ main.py
@@ -14,65 +14,43 @@ if es.ping():
 else:
     print("Failed to connect to Elasticsearch.")
 
+# Query entire episode transcripts
 def episode_from_query(es, query):
     resp = es.search(
         index="transcripts",
         pretty=True,
-        body={"query": {"match_phrase": {"transcript": query}}},
+        query={"match_phrase": {"transcript": query}},
     )
 
-    first_hit = resp["hits"]["hits"][0]
-    return first_hit['_source']['episode']
+    return resp["hits"]["hits"]
 
-def get_words_from_episode(es, episode, query):
+# Query section transripts from a specific episode
+def get_words_from_episode(es, episode_id, query):
     resp = es.search(
         index="words",
         pretty=True,
-        body={"query": {"match": {"episode": episode_id}}},
+        query={
+            "bool": {
+                "must": [
+                    {"term": {"episode.keyword": episode_id}},
+                    {"match": {"transcript": query }},
+                ]
+            }
+        },
     )
 
     return resp["hits"]["hits"]
 
 if __name__ == "__main__":
-    phrase_query = {
-        "query": {
-            "match_phrase" : {
-                "field": "SEarch"
-            }
-        }
-    }
-    intersection_query = {
-        "query": {
-            "bool": {
-                "must": [
-                    {"match": {"transcript": "Alice"}},
-                    {"match": {"time_stamp": "13:37"}}
-                ]
-            }
-        }
-    }
-
     s = "We don't know what it is or where it's going yet"
 
-    episode = episode_from_query(es, s)
+    episode_hits = episode_from_query(es, s)
+    print('first episode hit:', episode_hits[0]['_source']['episode'])
+    print('---------')
 
-    # ------------------------------------
-    # pseudo code for getting final results
-    # ------------------------------------
-
-    # def get_time_of_word(words, match):
-    #     for word in words:
-    #         if word == match:
-    #             return word.start_time
-    #     return None
-
-    # search_term = "I almost cried on the spot"
-    # id = resp.id 
-    # podcast_name = resp.podcast_name
-    # start_time = get_time_of_word(resp.words, search_term[0])
-    # returned_transcript = client.get(id - 1).transcript + resp.transcript + client.get(id + 1).transcript
-    # returned_start_time = start_time
-
-    # --------------------------------
-    # pseudo code ends
-    # ------------------------------
+    section_hits = get_words_from_episode(es, episode_hits['episode'], s)
+    for hit in section_hits:
+        print('hit:')
+        print(hit["_source"]["transcript"])
+        print('starts at', hit["_source"]["words"][0]['startTime'])
+        print('ends at', hit["_source"]["words"][-1]['endTime'])
